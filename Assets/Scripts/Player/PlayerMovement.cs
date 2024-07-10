74969fca864ada64d1f68d7ba04d77fb64033efb
PalamidaUsturoi <victor.t.nicola@gmail.com>
2024-07-10 16:48:21 +0300

    jump done

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,32 +8,24 @@ public class PlayerMovement : MonoBehaviour
   [SerializeField] private float m_MovementSpeed;
   [SerializeField] private float m_JumpSpeed;
   [SerializeField] private float m_RotationSpeed;
-
-  [Header("Raycast Settings")]
-	[SerializeField] private Vector3 m_BoxCastSize;
-	[SerializeField] private float m_CastLength;
 	[SerializeField] private LayerMask m_InteractionLayer;
-  [SerializeField] private Vector3 m_RaycastOffset;
 
   private Rigidbody m_Rigidbody;
   private Vector3 m_Movement;
   private bool m_JumpRequested;
-  private Vector3 m_RaycastSpawn;
+  private bool m_IsGrounded;
 
   void Awake()
   {
     m_Rigidbody = GetComponent<Rigidbody>();
+    m_IsGrounded = false;
   }
 
   void Update()
   {
-    m_RaycastSpawn = transform.position + m_RaycastOffset;
-    if (Physics.BoxCast(m_RaycastSpawn, m_BoxCastSize / 2, -transform.up, out RaycastHit raycastHit, transform.rotation, m_CastLength, m_InteractionLayer.value))
+    if (Input.GetButtonDown(m_JumpAxis) && m_IsGrounded)
     {
-      // if (Input.GetButtonDown(m_JumpAxis))
-      {
-        m_JumpRequested = true;
-      }
+      m_JumpRequested = true;
     }
 
     m_Movement = new Vector3(Input.GetAxis(m_HorizontalAxis), 0, Input.GetAxis(m_VerticalAxis));
@@ -54,8 +46,19 @@ public class PlayerMovement : MonoBehaviour
      m_Rigidbody.velocity = new Vector3(m_Movement.x * m_MovementSpeed, m_Rigidbody.velocity.y, m_Movement.z * m_MovementSpeed);
   }
 
-  void OnDrawGizmos()
+  void OnCollisionEnter(Collision collision)
   {
-    Gizmos.DrawCube(m_RaycastSpawn, m_BoxCastSize / 2);
+    if (1 << collision.collider.gameObject.layer == m_InteractionLayer.value)
+    {
+      m_IsGrounded = true;
+    }
+  }
+
+  void OnCollisionExit(Collision collision)
+  {
+    if (1 << collision.collider.gameObject.layer == m_InteractionLayer.value)
+    {
+      m_IsGrounded = false;
+    }
   }
 }
