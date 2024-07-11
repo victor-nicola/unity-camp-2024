77cefb8e3f88197e43b01b7a577c6d9e63dc233b
PalamidaUsturoi <victor.t.nicola@gmail.com>
2024-07-11 22:46:01 +0300

    esti un prost michael

---
+++ Assets/Scripts/Objects/LoudTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LoudTrigger : MonoBehaviour
+{
+  void Start()
+  {
+      
+  }
+
+  private void OnCollisionEnter(Collision collider) {
+
+  }
+}
